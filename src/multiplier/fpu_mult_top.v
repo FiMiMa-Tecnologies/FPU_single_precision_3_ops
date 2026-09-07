60a1f9a5523965a6c23a0afc57a92093f7dfc97b
MattGrossi12 <matheusgrossi2000@gmail.com>
2026-09-07 18:03:59 -0300

    Atualizei o topo com base nos novos sinais existentes, agora preciso testar tão somente o topo do multiplicador

--- src/multiplier/fpu_mult_top.v
+++ src/multiplier/fpu_mult_top.v
@@ -1,27 +1,30 @@
-module fpu_mult_top (
+module fpu_mult_top #(
 
     parameter   WIDTH   = 24,
                 T_WID   = (WIDTH - 1),
                 I_WID   = (T_WID -1),
                 R_WID   = ((WIDTH*2)-1),
-                EXP_WID = 7,
-                E_WID   = (EXP_WID - 1)
-                F_WID   = WIDTH + EXP_WID;
+                EXP_WID = 8,
+                E_WID   = (EXP_WID - 1),
+                F_WID   = WIDTH + EXP_WID - 1
 
 )(
     //Entradas
+    input   wire                clk, rst,
+    input   wire    [1:0]        op,
     input   wire    [E_WID:0]   a_e, b_e,
     input   wire                a_s, b_s,
     input   wire    [I_WID:0]   a_m, b_m,
 
     //Saídas:
+    output wire                 done,
     output wire     [F_WID:0]   result,
     output wire                 guard, round, sticky, overflow, underflow
 );
 
 wire [R_WID:0]  r_w;
-wire            exp_w;
-wire            sig_w,
+wire signed [EXP_WID:0] exp_w;
+wire            sig_w;
 wire [I_WID:0]  mant_w;
 
 fpu_mult_24x24 #(
@@ -31,46 +34,47 @@ fpu_mult_24x24 #(
                 .R_WID      (R_WID)
                 ) fpu_m2424 (
                 .a_m        (a_m), 
-                .b_m        (a_m),
-                .r_parc     (r_w)
+                .b_m        (b_m),
+                .clk        (clk),
+                .rst        (rst),
+                .op         (op),
+                .r_mant_s   (r_w),
+                .done       (done)
                 );
 
-fpu_exp_calc #(
+fpu_mult_exp_calc #(
                 .EXP_WID        (EXP_WID),
                 .E_WID          (E_WID)
                 ) fpu_me (
                 .a_e            (a_e), 
                 .b_e            (b_e),
-                .exp_final      (exp_w)
+                .clk            (clk),
+                .rst            (rst),
+                .exp_final      (exp_w),
+                .overflow       (overflow),
+                .underflow      (underflow)
             );
 
-fpu_norm #(
+fpu_mult_norm #(
                 .WIDTH          (WIDTH),
                 .T_WID          (T_WID),
                 .I_WID          (I_WID),
                 .R_WID          (R_WID)
                 ) fpu_nr (
-                .r_parc         (r_parc),
-                .mantissa       (mant_w)
+                .n_anorm        (r_w),
+                .n_norm         (mant_w),
+                .guard          (guard),
+                .round          (round),
+                .sticky         (sticky)
                 );
 
-fpu_norm_mux #(
-                .WIDTH          (WIDTH),
-                .T_WID          (T_WID),
-                .I_WID          (I_WID),
-                .R_WID          (R_WID)
-                ) fpu_nm (
-                .r_parc         (r_parc),
-                .mantissa       (mant_w)
-                );
-
-fpu_signal fpu_sg 
+fpu_mult_signal fpu_sg 
                 (
                 .a_s        (a_s), 
                 .b_s        (b_s),
-                .sig_final  (sig)
+                .sig_final  (sig_w)
                 );
 
-assign result = {sig_w, exp_w, mant_w};
+assign result = {sig_w, exp_w[E_WID:0], mant_w};
 
 endmodule: fpu_mult_top
